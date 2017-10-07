8c72814d43d0da94fa80d717e96001ca95dbc401
dmartel91 <d_sanchezmartel@hotmail.com>
2017-10-06 23:38:57 -0600

    using_ref_cursors_to_return_recordsets
    using_ref_cursors_to_return_recordsets

---
+++ using_ref_cursors_to_return_recordsets.sql
@@ -0,0 +1,39 @@
+SET serveroutput ON;
+DECLARE
+
+  l_cursor      SYS_REFCURSOR;
+  l_employee_id hr.employees.employee_id%type;
+  l_first_name  hr.employees.first_name%type;
+  l_last_name   hr.employees.last_name%type;
+  l_email       hr.employees.email%type;
+
+PROCEDURE get_cursor(p_first_value IN NUMBER
+                    ,p__last_value IN NUMBER
+                    ,p_result OUT SYS_REFCURSOR) AS
+BEGIN
+
+  OPEN p_result FOR
+  SELECT e.employee_id
+        ,e.first_name
+        ,e.last_name
+        ,e.email
+    FROM employees e
+   WHERE 1 = 1
+     AND e.employee_id BETWEEN p_first_value AND p__last_value;
+
+END get_cursor;
+
+BEGIN
+  
+  get_cursor(200, 206, l_cursor);
+
+  LOOP
+    FETCH l_cursor INTO l_employee_id, l_first_name, l_last_name, l_email;
+    EXIT WHEN l_cursor%NOTFOUND;
+
+  DBMS_OUTPUT.PUT_LINE(RPAD(l_employee_id, 10) || RPAD(l_first_name, 10) || RPAD(l_last_name, 10) || RPAD(l_email, 10));
+  END loop;
+
+  CLOSE l_cursor;
+
+END;
